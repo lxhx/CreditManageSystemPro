5abc3dde5ec61e7e0fdbdb9c9ed233c044b7fd7f
lgRogen <651021137@qq.com>
2016-05-26 16:37:00 +0800

    Global.asax.cs

--- CreditManageSystemPro/Global.asax.cs
+++ CreditManageSystemPro/Global.asax.cs
@@ -30,6 +30,11 @@ namespace CreditManageSystemPro
                 namespaces:new string[] { "CreditManageSystemPro.Admin.Controllers" }
             ).DataTokens.Add("Area","Admin");
         }
+
+
+
+
+
         protected void Application_Start()
         {
             Database.SetInitializer<CreditManageContext>(null);
